e3e10d6d283481fb6683509a02de6afb119ec7a0
luis-pc <95900211+LuisAguilarG@users.noreply.github.com>
2022-02-09 12:39:00 -0600

    Corrección de logo

--- docs/assets/js/boton.js
+++ docs/assets/js/boton.js
@@ -4,7 +4,7 @@ var mostrarMensaje = () => {
     <h1 class="titulo letras-estilo3">Bien hecho</h2>
     <div class="card-body">
       <p>
-        Jessie: Prepárense para los problemas. <br> James: Y más vale que teman. Jessie: Para proteger al mundo de la
+        Jessie: Prepárense para los problemas. <br> James: Y más vale que teman. <br> Jessie: Para proteger al mundo de la
         devastación. <br> James: Para unir a los pueblos dentro de nuestra nación. <br> Jessie: Para denunciar los males de la
         verdad y el amor. <br> James: Para extender nuestro reino hasta las estrellas. <br> Jessie: ¡Jessie!  <br> James: ¡Jame me me
         mes! <br> Jessie: ¡El Equipo Rocket viajando a la velocidad de la luz! <br> James: ¡Ríndanse ahora o prepárense a
