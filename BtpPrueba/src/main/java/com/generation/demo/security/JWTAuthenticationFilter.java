93651d9a1741e8e3f6916a5f9ae106a6893f9d11
Javier de Jesús Pérez Santiago <javier.de.jesus.perez.santiago@gmail.com>
2022-03-02 13:36:31 -0600

    2 de marzo 01:36

---
+++ BtpPrueba/src/main/java/com/generation/demo/security/JWTAuthenticationFilter.java
@@ -0,0 +1,55 @@
+package com.generation.demo.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.generation.demo.model.UsuarioModel;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
+import static com.generation.demo.security.Constants.*;
+
+public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {        
+    private static final String ISSUER_INFO = null;
+	private AuthenticationManager authenticationManager;
+
+    public JWTAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)     
+            throws AuthenticationException {
+        try {
+            UsuarioModel credentials = new ObjectMapper().readValue(request.getInputStream(), UsuarioModel.class);  
+
+            return authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(    
+                    credentials.getCorreo(), credentials.getContrasenia(), new ArrayList<>()));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override                                                                                                                        
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+                                            Authentication auth) throws IOException, ServletException {
+
+        String token = Jwts.builder().setIssuedAt(new Date()).setIssuer(ISSUER_INFO)                                     
+                .setSubject(((org.springframework.security.core.userdetails.User)auth.getPrincipal()).getUsername())
+                .setExpiration(new Date(System.currentTimeMillis() + TOKEN_EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS512, SUPER_SECRET_KEY).compact();
+        response.addHeader(HEADER_AUTHORIZACION_KEY, TOKEN_BEARER_PREFIX + token);
+    }
+}
